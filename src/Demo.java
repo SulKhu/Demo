5aa7f303eccba994f47bbe547570a34c7a5eb7db
KHURRUM <s-skhurrum@lwsd.org>
2022-04-01 16:44:20 -0700

    Initial Commit

---
+++ src/Demo.java
@@ -0,0 +1,24 @@
+// Written by Sulaimaan K
+// A demonstration of basic Git commands
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		// declare 4 variables
+		// TODO: Switch out the values for each variable to match your personal preferences
+		
+		int myFavoriteInteger = 19;
+		double myLeastFavoriteDouble = 0.333;
+		boolean camelCase = true;
+		char firstInitial = 'S';
+		
+		
+		// print out each variable
+		System.out.println(myFavoriteInteger);
+		System.out.println(myLeastFavoriteDouble);
+		System.out.println(camelCase);
+		System.out.println(firstInitial);
+		
+	}
+}
